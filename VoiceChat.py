735cb39df6fa71350b1cbdcb226db40bbb2e7332
come24will@gmail.com <come24will@gmail.com>
2024-11-14 17:53:19 -0500

    Everything up to the point it tries to play the track works. I can't get the VoiceClient object to play the track and I'm not sure why.

--- VoiceChat.py
+++ VoiceChat.py
@@ -13,10 +13,8 @@ from queue import Queue
 from Song import Song
 import ffmpeg
 import time
-from Effects import Effects
 
-#ffmpeg path in cwd, temporary solution
-audiopath = './ffmpeg-2024-07-04-git-03175b587c-full_build/bin'
+
 
 #initializing class
 class VoiceChat(commands.Cog):
@@ -24,12 +22,15 @@ class VoiceChat(commands.Cog):
         #bot instance
         self.bot = bot
         #music queue containing song name and file location on local system
-        self.queue = Queue()
-        self.index = 0
+        self.pathqueue = Queue()
+        self.titlequeue = Queue()
         #paused boolean, important for play() and paused()
         self.paused = False
         #playing boolean
         self.playing = False
+        #ffmpeg path in cwd, temporary solution
+        self.audiopath = './ffmpeg-2024-07-04-git-03175b587c-full_build/bin'
+        self.audioexe = './ffmpeg-2024-07-04-git-03175b587c-full_build/bin/ffmpeg.exe'
         #keeps track of the channel ID the bot is in
         self.channel = {}
         #indicates if the bot has joined a channel
@@ -39,13 +40,19 @@ class VoiceChat(commands.Cog):
         #runtime of currently playing track. useful when applying or undo-ing effects so that the bot can remember where it left off
         self.runtime = 0
         #options for yt-dlp. indicates download as mp3, doesn't download playlist links, downloads highest-res audio possible
-        self.ytoptions = {'outmpath': './resources', 'noplaylist': 'True', 'format': 'bestaudio', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}], 'ffmpeg_location': audiopath}
+        self.ytoptions = {'outmpath': './resources', 'noplaylist': 'True', 'format': 'bestaudio', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}], 'ffmpeg_location': self.audiopath}
         self.ffmpegoptions = {'before_options': 'reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        #audio play
+        self.aplay = 0
+        self.voicestatus = {}
+
+
 
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("placeholder")
+        for guild in self.bot.guilds:
+           self.id = int(guild.id)
 
 
         #join voice chat function
@@ -62,12 +69,21 @@ class VoiceChat(commands.Cog):
              self.joined = True
              await ctx.send("Connecting..")
              self.channel[id] = await channel.connect()
+             path = "https://www.youtube.com/watch?v=-1QSokJSb5A"
+             self.channel[id].play(discord.FFmpegPCMAudio(path, executable=self.audioexe))
+             print("T")
         else:
             await ctx.send("Please join a voice channel.")
 
         #leave voice chat function
         #NOTE: voice_leave is not functional. my code refuses to acknowledge the VoiceClient object it needs to disconnect
         #from the channel. Everything up to "await voicechan.disconnect()" works as expected, AFAIK.
+
+    def findChannel(self, bot, voice_channel: discord.VoiceChannel):
+            for voice_client in self.bot.voice_clients:
+               if voice_client.channel.id == voice_channel.id:
+                  return voice_client
+        
     @commands.command(name="leave")
     async def voice_leave(self, ctx: commands.Context):
         #if bot is in a voice channel, and isn't playing any songs:
@@ -75,13 +91,12 @@ class VoiceChat(commands.Cog):
             if(self.paused == False and self.playing == False and self.joined):
                 self.joined = False
                 channel = ctx.author.voice.channel
+                client = self.findChannel(self.bot, channel)
+                self.voicestatus = client
                 keyguild = ctx.guild
-                #searches all channels the bot is in for the one that matches server ID, disconnects from it
-                for client in self.bot.voice_clients:
-                    voicechan = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-                    print(voicechan)
-                    await ctx.send("Disconnecting..")
-                    await voicechan.disconnect()
+                await ctx.send("Disconnecting..")
+                await self.voicestatus.disconnect(force=True)
+
         else:
             await ctx.send("Failed to leave channel.")
         self.channel = None
@@ -97,40 +112,30 @@ class VoiceChat(commands.Cog):
     async def pause(self, ctx: commands.Context):
         self.paused = True
         self.playing = False
+        #ctx.voice_client.pause(self.aplay)
 
     @commands.command(name = "unpause")
     async def unpause(self, ctx: commands.Context):
        self.paused = False
        self.playing = True
+       #ctx.voice_client.play(self.aplay)
 
         #play song function
     @commands.Cog.listener()
     async def play(self, ctx: commands.Context):
+       id = int(ctx.guild.id)
        #sleeps until current track is done playing
        await ctx.send("INPLAY")
        while(self.joined == True):
          while(self.playing == True):
             time.sleep(1)
        #iterates through the entire queue in order until none are left, sets playing to true
-         while(self.queue.qsize() > 0):
-            s = self.queue.get()
-            print(s.title())
-            print("T35235")
-            aplay = discord.FFmpegPCMAudio(executable="ffmpeg", source=s.path())
-            ctx.voice_client.play(aplay)
-            #s.link.play()
+         while(self.pathqueue.qsize() > 0 and self.pathqueue.qsize() == self.titlequeue.qsize()):
+            path = self.pathqueue.get()
+            title = self.titlequeue.get()
+            self.channel.play(discord.FFmpegPCMAudio(path, executable=self.audioexe))
+            await ctx.send("Now Playing: {}".format(title))
             self.playing = True
-        
-
-        #apply effects function, will reference effects cog
-    @commands.command(name = "fx")
-    async def fx(self, ctx: commands.Context, effect):
-        if(self.playing == False):
-            ctx.send("Music needs to be playing for effects to be applied.")
-        else:
-            #effects applied here
-            print("Placeholder")
-
     
 
         #downloads youtube videos, creates Song object, places object in the queue and sets it to be played 
@@ -142,16 +147,11 @@ class VoiceChat(commands.Cog):
       with yt_dlp.YoutubeDL(self.ytoptions) as youtube:
             try:
              #downloads video and metadata, converts to MP3
-             songinfo = youtube.extract_info(url, download = True)
+             songinfo = youtube.extract_info(url, download = False)
              #gets filename
-             songpath = youtube.prepare_filename(songinfo)
-             #concatenates cd for full path
-             songpath = "./" + songpath
-             #gets title from metadata
              title = songinfo.get('title', None)
-             #new song object made from downloaded video put into the queue
-             so = Song(title,songpath)
-             self.queue.put(so)
+             self.pathqueue.put(url)
+             self.titlequeue.put(title)
              await ctx.send("Song Title: {}".format(title))
              if not(self.playing):
               await self.play(ctx)
